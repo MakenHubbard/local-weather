85870d8bb39bc009d288c1408befb295be630d03
Maken Hubbard <makhub26@outlook.com>
2018-05-30 20:16:50 -0500

    have the five day data coming back

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,5 +1,92 @@
+const moment = require('../lib/node_modules/moment');
 
-// const domString = () => {
-//   let strung = '';
+const domString = (results, day) => {
+  let strung = '';
+  if (day === 1) {
+    strung += `<div class="row">`;
+    strung += `<div class="col-sm-6 col-md-6">`;
+    strung += `<div class="thumbnail">`;
+    strung += `<h1 align="center">${moment().format('<b>dddd</b> </br> MMMM do, YYYY')}</h1>`;
+    strung += `<div class="caption">`;
+    strung += `<p align="center">Average Temperature: <b>${Math.ceil(results.main.temp)} F</p>`;
+    strung += `<p><strong>Conditions:</strong> ${results.weather[0].description} <img src="http://openweathermap.org/img/w/${results.weather[0].icon}.png"> </p>`;
+    strung += `<ul type="circle">`;
+    strung += `<li>Humidity: ${results.main.humidity}%</li>`;
+    strung += `<li>Barometric Pressure: ${results.main.pressure} hPa</li>`;
+    strung += `<li>Temp. High: ${Math.ceil(results.main.temp_max)} F</li>`;
+    strung += `<li>Temp. Low: ${Math.ceil(results.main.temp_min)} F</li>`;
+    strung += `<li>Wind Speed: ${Math.ceil(results.wind.speed)} mph</li>`;
+    strung += `</ul>`;
+    strung += `<p><a href="#" id="5-days-btn" class="btn btn-primary" role="button">5-Day Forecast</a></p>`;
+    strung += `</div>`;
+    strung += `</div>`;
+    strung += `</div>`;
+    strung += `</div>`;
 
-// }
+  } else {
+    console.error('rsu;t',results);
+    results.list.forEach((result) => {
+      strung += `<div class="row">`;
+      strung += `<div class="col-sm-6 col-md-6">`;
+      strung += `<div class="thumbnail">`;
+      strung += `<h1 align="center">${moment().format('<b>dddd</b> </br> MMMM do, YYYY')}</h1>`;
+      strung += `<div class="caption">`;
+      strung += `<p align="center">Average Temperature: <b>${Math.ceil(result.main.temp)} F</p>`;
+      strung += `<p><strong>Conditions:</strong> ${result.weather[0].description} <img src"http://openweathermap.org/img/w/${result.weather[0].icon}.png"> </p>`;
+      strung += `<ul type="circle">`;
+      strung += `<li>Humidity: ${result.main.humidity}%</li>`;
+      strung += `<li>Barometric Pressure: ${result.main.pressure} hPa</li>`;
+      strung += `<li>Temp. High: ${Math.ceil(result.main.temp_max)} F</li>`;
+      strung += `<li>Temp. Low: ${Math.ceil(result.main.temp_min)} F</li>`;
+      strung += `<li>Wind Speed: ${Math.ceil(result.wind.speed)} mph</li>`;
+      strung += `</ul>`;
+      strung += `<p><a href="#" class="btn btn-primary" role="button">5-Day Forecast</a></p>`;
+      strung += `</div>`;
+      strung += `</div>`;
+      strung += `</div>`;
+      strung += `</div>`;
+    });
+  };
+  printOneDayToDom(strung);
+};
+
+// const damString = (results, day) => {
+//   let string = '';
+//   if (day === 5) {
+//     results.forEach((result) => {
+//     string += `<div class="row">`;
+//     string += `<div class="col-sm-6 col-md-6">`;
+//     string += `<div class="thumbnail">`;
+//     string += `<h1 align="center">${moment().format('<b>dddd</b> </br> MMMM do, YYYY')}</h1>`;
+//     string += `<div class="caption">`;
+//     string += `<p align="center">Average Temperature: <b>${Math.ceil(result.main.temp)} F</p>`;
+//     string += `<p><strong>Conditions:</strong> ${result.weather[0].description} <img src="http://openweathermap.org/img/w/${result.weather[0].icon}.png"> </p>`;
+//     string += `<ul type="circle">`;
+//     string += `<li>Humidity: ${result.main.humidity}%</li>`;
+//     string += `<li>Barometric Pressure: ${result.main.pressure} hPa</li>`;
+//     string += `<li>Temp. High: ${Math.ceil(result.main.temp_max)} F</li>`;
+//     string += `<li>Temp. Low: ${Math.ceil(result.main.temp_min)} F</li>`;
+//     string += `<li>Wind Speed: ${Math.ceil(result.wind.speed)} mph</li>`;
+//     string += `</ul>`;
+//     string += `<p><a href="#" class="btn btn-primary" role="button">5-Day Forecast</a></p>`;
+//     string += `</div>`;
+//     string += `</div>`;
+//     string += `</div>`;
+//     string += `</div>`;
+//   });
+//     printFiveDayToDom(string);
+//   };
+// };
+
+const printOneDayToDom = (result) => {
+  $('#report').html(result);
+};
+
+// const printFiveDayToDom = (result) => {
+//   $('#report').html(result)
+// };
+
+module.exports = {
+  domString,
+  // damString,
+};
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,16 +1,21 @@
 const lwdb = require('./lwdb');
 
 const searchZip = () => {
+
   $('#zipCode').click((e) => {
     const userInput = $('#userZip').val();
-    lwdb.showOneDay(userInput);
+    lwdb.showOneDay(userInput,1);
     console.error(userInput);
   });
   $(document).keypress((e) => {
     const userInput = $('#userZip').val();
     if (e.key === 'Enter') {
-      lwdb.showOneDay(userInput);
+      lwdb.showOneDay(userInput,1);
     };
+  });
+  $(document).on('click','#5-days-btn', (e) => {
+    const userInput = $('#userZip').val();
+    lwdb.showOneDay(userInput,5);
   });
 };
 
