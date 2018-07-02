85aa0583bf8924793faf3132912717afa444ce7d
Maken Hubbard <makhub26@outlook.com>
2018-07-02 17:55:35 -0500

    adding some update code to continue work on wip

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -1,17 +1,51 @@
-// let uid = '';
+let uid = '';
 
-// const getUid = () => {
-//   return uid;
-// };
+const getUid = () => {
+  return uid;
+};
 
-// const setUid = () => {
-//   uid = newUid;
-// };
+const setUid = () => {
+  uid = newUid;
+};
 
-// const loginInStatus = () => {
-//   firebas.auth().onAuthStateChanged((user) => {
-//     if(user) {
-//       setUid(user.uid);
-//     };
-//   });
-// };
+const authEvents = () => {
+  $('#signIn-btn').click((e) => {
+    e.preventDefault();
+    const email = $('#inputEmail').val();
+    const pass = $('#inputPassword').val();
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+      .catch((error) => {
+        $('#signin-error-msg').text(error.message);
+        $('#signin-error').removeClass('hide');
+        const errorMessage = error.message;
+        console.error(errorMessage);
+      });
+  });
+
+const checkLoginInStatus = () => {
+  firebas.auth().onAuthStateChanged((user) => {
+    if(user) {
+      setUid(user.uid);
+      $('#theSearchPart').removeClass('hide');
+      $('#weatherReportDiv').removeClass('hide');
+      $('#registration-form').addClass('hide');
+      $('#authScreen').addClass('hide');
+      $('#myWeather').addClass('hide');
+    } else {
+      $('#theSearchPart').addClass('hide');
+      $('#weatherReportDiv').addClass('hide');
+      $('#registration-form').addClass('hide');
+      $('#authScreen').removeClass('hide');
+      $('#myWeather').addClass('hide');
+    }
+  });
+};
+
+const initializer = () => {
+  authEvents();
+  checkLoginInStatus();
+};
+
+module.exports = {
+  initializer,
+};
